cd9b609e07651f91282328753ba0c05af452b568
Vitaliy <si_vitalik@yahoo.com>
2017-03-17 16:20:23 +0200

    Ceate model category and model product

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Product
+
+# Model category
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+# Model product
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
-# Модель категории
+
+# model category
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
@@ -16,25 +18,34 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:ProductListByCategory', args=[self.slug])
 
-# Модель продукта
+
+# Product model
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', verbose_name="Категорія")
-    name = models.CharField(max_length=200, db_index=True, verbose_name="Назва")
+    category = models.ForeignKey(Category, related_name='products', verbose_name=u"Категорія")
+    name = models.CharField(max_length=200, db_index=True, verbose_name=u"Назва")
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, verbose_name="Зображення товару")
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, verbose_name=u"Зображення товару")
     description = models.TextField(blank=True, verbose_name="Опис")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Ціна")
-    stock = models.PositiveIntegerField(verbose_name="На складі")
-    available = models.BooleanField(default=True, verbose_name="Доступний")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=u"Ціна")
+    stock = models.PositiveIntegerField(verbose_name=u"На складі")
+    available = models.BooleanField(default=True, verbose_name=u"Доступний")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['name']
+        verbose_name = u'Назва товару'
+        verbose_name_plural = u'Назви товарів'
+
         index_together = [
             ['id', 'slug']
         ]
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:ProductDetail', args=[self.id, self.slug])
